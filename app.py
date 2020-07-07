5ac5d27ccba758ca5fef0d0e3166c4eb4af9dab9
Woodam Lim <woodam.lim@gmail.com>
2020-07-08 01:28:07 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import csv
+import json
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
